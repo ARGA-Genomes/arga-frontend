c6883a0f319a94fd400e0da378ea9b0d246754e7
Goran Sterjov <secure@goran.email>
2023-02-07 19:46:44 +1100

    made browse data section responsive

--- src/app/browse-data.tsx
+++ src/app/browse-data.tsx
@@ -4,8 +4,6 @@ import { gql, useQuery } from '@apollo/client';
 
 import { Grid } from "@mantine/core";
 import { BrowseCard, BrowseCardLoading } from "./browse-card";
-import Loading from './loading';
-
 
 type Overview = {
   publishedDatasets: number,
@@ -40,22 +38,22 @@ export default function BrowseData() {
 
   return (
       <Grid>
-        <Grid.Col span={4}>
+        <Grid.Col xs={12} sm={12} md={4} lg={4} xl={4}>
           {loading || !data ? <BrowseCardLoading/> : <BrowseCard category="Published datasets" total={data.overview.publishedDatasets} />}
         </Grid.Col>
-        <Grid.Col span={4}>
+        <Grid.Col xs={12} sm={12} md={4} lg={4} xl={4}>
           {loading || !data ? <BrowseCardLoading/> : <BrowseCard category="Bacteria specimens" total={data.overview.bacteria} />}
         </Grid.Col>
-        <Grid.Col span={4}>
+        <Grid.Col xs={12} sm={12} md={4} lg={4} xl={4}>
           {loading || !data ? <BrowseCardLoading/> : <BrowseCard category="Australian specimens" total={data.overview.inAustralia} />}
         </Grid.Col>
-        <Grid.Col span={4}>
+        <Grid.Col xs={12} sm={12} md={4} lg={4} xl={4}>
           {loading || !data ? <BrowseCardLoading/> : <BrowseCard category="Marine specimens" total={data.overview.marine} />}
         </Grid.Col>
-        <Grid.Col span={4}>
+        <Grid.Col xs={12} sm={12} md={4} lg={4} xl={4}>
           {loading || !data ? <BrowseCardLoading/> : <BrowseCard category="Terrestrial specimens" total={data.overview.terrestrial} />}
         </Grid.Col>
-        <Grid.Col span={4}>
+        <Grid.Col xs={12} sm={12} md={4} lg={4} xl={4}>
           {loading || !data ? <BrowseCardLoading/> : <BrowseCard category="Preserved specimens" total={data.overview.preservedSpecimens} />}
         </Grid.Col>
       </Grid>
--- src/app/loading.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { Loader } from "@mantine/core";
-
-export default function Loading() {
-  return <>Loading...</>
-    /* return <Loader color="moss" variant="bars" />; */
-}
