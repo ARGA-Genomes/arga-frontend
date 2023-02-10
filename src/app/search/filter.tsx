592658afc207c1413e45cf6f6681bd3be6a2d028
Goran Sterjov <secure@goran.email>
2023-02-10 14:24:39 +1100

    now using taxonomy filters provided by the backend

--- src/app/search/filter.tsx
+++ src/app/search/filter.tsx
@@ -1,50 +1,114 @@
 'use client';
 
-import { Paper, Chip, Autocomplete, Badge, ActionIcon, Button, Collapse, Divider, SimpleGrid } from "@mantine/core";
+import { gql, useQuery } from "@apollo/client";
+import { Paper, Chip, Autocomplete, Badge, ActionIcon, Button, Collapse, Divider, SimpleGrid, Select } from "@mantine/core";
 import { useState } from "react";
 import { Plus, X } from "tabler-icons-react";
 
 
-enum Kingdom {
-  Bacteria,
-  Animalia,
-  Protista,
-  Fungi,
-  Protozoa,
-  Archea,
-  Plantae,
-  Chromista,
-  Eukaryota,
-  Virus,
+const GET_TAXONOMY_FILTERS = gql`
+query {
+  search {
+    filters {
+      taxonomy {
+        kingdom {
+          totalMatches
+          values {
+            matches
+            value
+          }
+        }
+
+        phylum {
+          totalMatches
+          values {
+            matches
+            value
+          }
+        }
+
+        class {
+          totalMatches
+          values {
+            matches
+            value
+          }
+        }
+
+        family {
+          totalMatches
+          values {
+            matches
+            value
+          }
+        }
+
+        genus {
+          totalMatches
+          values {
+            matches
+            value
+          }
+        }
+      }
+    }
+  }
+}
+`
+
+type FilterOption = {
+  matches: number,
+  value?: string,
 };
-const KingdomTypes = [
-  "Bacteria",
-  "Animalia",
-  "Protista",
-  "Fungi",
-  "Protozoa",
-  "Archea",
-  "Plantae",
-  "Chromista",
-  "Eukaryota",
-  "Virus",
-];
+
+type Filter = {
+  totalMatches: number,
+  values: FilterOption[],
+};
+
+type TaxonomyFilters = {
+  kingdom: Filter,
+  phylum: Filter,
+  class: Filter,
+  family: Filter,
+  genus: Filter,
+};
+
+type Filters = {
+  taxonomy: TaxonomyFilters,
+};
+
+type SearchResults = {
+  filters: Filters,
+};
+
+type QueryResults = {
+  search: SearchResults,
+};
 
 type FilterParams = {
-  kingdom?: Kingdom[],
-  phylum?: string[],
-  class?: string[],
-  family?: string[],
-  genus?: string[],
+  kingdom: Set<string>,
+  phylum: Set<string>,
+  class: Set<string>,
+  family: Set<string>,
+  genus: Set<string>,
 };
 
-const FilterTypes = [
-  "Kingdom",
-  "Phylum",
-  "Class",
-  "Family",
-  "Genus",
-];
+
+type AutocompleteData = {
+  value: string,
+  filter: Filter,
+}
+
+function wrapFilterData (filters: TaxonomyFilters): AutocompleteData[] {
+  return [
+    { value: "Kingdom", filter: filters.kingdom },
+    { value: "Phylum", filter: filters.phylum },
+    { value: "Class", filter: filters.class },
+    { value: "Family", filter: filters.family },
+    { value: "Genus", filter: filters.genus },
+  ]
+}
 
 
 type FilterBarProps = {
@@ -56,18 +120,51 @@ function FilterBar(props: FilterBarProps) {
   const [type, setType] = useState("");
   const [val, setVal] = useState("");
 
+  const [rank, setRank] = useState<AutocompleteData | null>(null);
+
   const addFilter = (val: string) => {
-    setVal(val);
-    props.onAddFilter(type, val);
+    if (!rank) return;
+
+    props.onAddFilter(rank.value, val);
+    setVal("");
+    setType("");
+    setRank(null);
+    setShow(false);
   };
+
+  const addTaxonomy = (val: AutocompleteData) => {
+    setRank(val);
+  };
+
+  const { loading, error, data } = useQuery<QueryResults>(GET_TAXONOMY_FILTERS);
+  if (loading) return <>Loading..</>
+  if (error) return <>Error: {error}</>
+  if (!data) return <>No data</>;
 
   return (
     <SimpleGrid cols={1}>
       <Button onClick={() => setShow(show => !show)}><Plus />Add filter</Button>
 
       <Collapse in={show}>
-        <Autocomplete placeholder="Filter type" value={type} onChange={setType} data={FilterTypes} />
-        <Autocomplete placeholder="Filter value" value={val} onChange={addFilter} data={KingdomTypes} disabled={type ? false : true} />
+        <Autocomplete
+          placeholder="Taxonomy"
+          onItemSubmit={addTaxonomy}
+          data={wrapFilterData(data.search.filters.taxonomy)}
+          limit={300}
+        />
+        {rank ? (
+          <Select
+            placeholder={`Filter by ${rank.value}`}
+            searchable
+            clearable
+            nothingFound="No options"
+            maxDropdownHeight={280}
+            onChange={addFilter}
+            data={rank.filter.values.map(item => { return { label: item.value || "", matches: item.matches, value: item.value || "" }})}
+          />
+        ) : (
+          null
+        )}
       </Collapse>
     </SimpleGrid>
   );
@@ -97,18 +194,34 @@ function FilterTag(props: FilterTagProps) {
 
 
 export default function SearchFilter() {
-  const [params, setParams] = useState<FilterParams>({ kingdom: [Kingdom.Animalia] });
+  const [params, setParams] = useState<FilterParams>({
+    kingdom: new Set(),
+    phylum: new Set(),
+    class: new Set(),
+    family: new Set(),
+    genus: new Set(),
+  });
 
-  const setFilter = (_type: string, val: string) => {
-    const filter: FilterParams = { phylum: (params.phylum || []).concat([val]) };
-    setParams({...filter, ...params});
+  const setFilter = (rank: string, val: string) => {
+    const filter: FilterParams = {
+      ...params,
+      kingdom: rank == "Kingdom" ? new Set([...params.kingdom, val]) : params.kingdom,
+      phylum: rank == "Phylum" ? new Set([...params.phylum, val]) : params.phylum,
+      class: rank == "Class" ? new Set([...params.class, val]) : params.class,
+      family: rank == "Family" ? new Set([...params.family, val]) : params.family,
+      genus: rank == "Genus" ? new Set([...params.genus, val]) : params.genus,
+    };
+    setParams(filter);
   }
 
   return (
     <Paper bg="#e7f4ff" p="lg" radius="md" shadow="xl" withBorder>
       <Chip.Group>
-        {params.kingdom?.map(filter => <FilterTag type="Kingdom" value={Kingdom[filter]} key={filter} />)}
-        {params.phylum?.map(filter => <FilterTag type="Phylum" value={filter} key={filter} />)}
+        {[...params.kingdom].map(filter => <FilterTag type="Kingdom" value={filter} key={filter} />)}
+        {[...params.phylum].map(filter => <FilterTag type="Phylum" value={filter} key={filter} />)}
+        {[...params.class].map(filter => <FilterTag type="Class" value={filter} key={filter} />)}
+        {[...params.family].map(filter => <FilterTag type="Family" value={filter} key={filter} />)}
+        {[...params.genus].map(filter => <FilterTag type="Genus" value={filter} key={filter} />)}
       </Chip.Group>
       <Divider my="md" />
       <FilterBar onAddFilter={setFilter} />
