c4a7de49c483c23a9c6077673357be46ca974be6
dewmini <5234594+dewmini@users.noreply.github.com>
2023-06-01 13:58:21 +1000

    Add whole genome changes

--- src/app/species/[name]/wholeGenome/wholeGenomeDetails.tsx
+++ src/app/species/[name]/wholeGenome/wholeGenomeDetails.tsx
@@ -5,6 +5,7 @@ import { QueryResults} from "@/app/type";
 import {gql, useQuery} from "@apollo/client";
 import {GenomeRecords} from "@/app/species/[name]/genomeRecords";
 import {Box} from "@mantine/core";
+import Link from "next/link";
 
 
 const GET_SPECIES = gql`
@@ -48,6 +49,8 @@ export function WholeGenomeDetails({ speciesName }: { speciesName : String }) {
   const referenceGenome = data.species.data.filter((record) => record.refseqCategory == "reference genome"
     || record.accession?.includes("GCF_"));
 
+  const link = referenceGenome.map((record => record.accessionUri))[0] ?? "";
+
   return (
   <>
     <Grid p={40}>
@@ -61,7 +64,7 @@ export function WholeGenomeDetails({ speciesName }: { speciesName : String }) {
                   <Text color="white" style={{ padding: 15 }}>Species Name: {speciesName?.replaceAll("_", " ")}</Text>
                   <h3 color="white" style={{ padding: 15 }}>Reference Genome</h3>
                   <Text color="white" style={{ padding: 15 }}>Refseq: {referenceGenome.map((record => record.accession))}</Text>
-                  <Text color="white" style={{ padding: 15 }}>{referenceGenome.map((record => record.accessionUri))}</Text>
+                  <Link href={link} target="_blank"><Text color="white" style={{ padding: 15 }}>{link}</Text></Link>
                 </Paper>
               </Stack>
               <br/>
