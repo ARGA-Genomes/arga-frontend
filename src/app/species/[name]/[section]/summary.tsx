df259773f90517189ed478d1cb696b17aee2265e
Jack Brinkman <jack.brinkman@csiro.au>
2023-07-06 12:27:56 +1000

    Added 'view on ALA' button to species header, changed summary page card radii

--- src/app/species/[name]/[section]/summary.tsx
+++ src/app/species/[name]/[section]/summary.tsx
@@ -1,67 +1,74 @@
-'use client';
+"use client";
 
 import { gql, useQuery } from "@apollo/client";
-import { Box, Grid, Group, Image, LoadingOverlay, Paper, Stack, Text } from "@mantine/core";
-import { Photo, Taxonomy, Regions, StatsSpecies, Species} from "@/app/type";
+import {
+  Box,
+  Grid,
+  Group,
+  Image,
+  LoadingOverlay,
+  Paper,
+  Stack,
+  Text,
+} from "@mantine/core";
+import { Photo, Taxonomy, Regions, StatsSpecies, Species } from "@/app/type";
 import Link from "next/link";
 import dynamic from "next/dynamic";
 
-
 const GET_SUMMARY = gql`
-query SpeciesSummary($canonicalName: String) {
-  stats {
+  query SpeciesSummary($canonicalName: String) {
+    stats {
+      species(canonicalName: $canonicalName) {
+        total
+        wholeGenomes
+        mitogenomes
+        barcodes
+      }
+    }
     species(canonicalName: $canonicalName) {
-      total
-      wholeGenomes
-      mitogenomes
-      barcodes
-    }
-  }
-  species(canonicalName: $canonicalName) {
-    taxonomy {
-      canonicalName
-      authorship
-      kingdom
-      phylum
-      class
-      order
-      family
-      genus
-      vernacularGroup
-    }
-    photos {
-      url
-      referenceUrl
-      publisher
-      license
-      rightsHolder
-    }
-    regions {
-      ibra {
-        name
+      taxonomy {
+        canonicalName
+        authorship
+        kingdom
+        phylum
+        class
+        order
+        family
+        genus
+        vernacularGroup
       }
-      imcra {
-        name
+      photos {
+        url
+        referenceUrl
+        publisher
+        license
+        rightsHolder
+      }
+      regions {
+        ibra {
+          name
+        }
+        imcra {
+          name
+        }
       }
     }
   }
-}`;
-
+`;
 
 type StatsResults = {
-  species: StatsSpecies,
-}
-
-type QueryResults = {
-  stats: StatsResults,
-  species: Species,
+  species: StatsSpecies;
 };
 
+type QueryResults = {
+  stats: StatsResults;
+  species: Species;
+};
 
-const RegionMap = dynamic(() => import('../../../components/region-map'), {
+const RegionMap = dynamic(() => import("../../../components/region-map"), {
   ssr: false,
   loading: () => <Text>Loading map...</Text>,
-})
+});
 
 function SpeciesPhoto({ photo }: { photo: Photo }) {
   function small(url: string) {
@@ -70,78 +77,118 @@ function SpeciesPhoto({ photo }: { photo: Photo }) {
 
   return (
     <Box>
-      <Image width={300} height={300} radius="lg" src={small(photo.url)} alt="Species image" />
-      <Text fz="sm" c="dimmed">&copy; { photo.rightsHolder }</Text>
-      <Text fz="sm" c="dimmed"><Link href={ photo.referenceUrl || "#" } target="_blank">{ photo.publisher }</Link></Text>
+      <Image
+        width={300}
+        height={300}
+        radius="lg"
+        src={small(photo.url)}
+        alt="Species image"
+      />
+      <Text fz="sm" c="dimmed">
+        &copy; {photo.rightsHolder}
+      </Text>
+      <Text fz="sm" c="dimmed">
+        <Link href={photo.referenceUrl || "#"} target="_blank">
+          {photo.publisher}
+        </Link>
+      </Text>
     </Box>
-  )
+  );
 }
 
 function DataSummary({ stats }: { stats: StatsSpecies }) {
   return (
-    <Paper bg="midnight.6" radius="xl" py={20} px={40}>
+    <Paper bg="midnight.6" radius="lg" p="lg">
       <Group position="apart">
         <Text color="white">Whole Genomes</Text>
-        <Text color="white" fw={700} fz={30}>{stats.wholeGenomes}</Text>
+        <Text color="white" fw={700} fz={30}>
+          {stats.wholeGenomes}
+        </Text>
       </Group>
       <Group position="apart">
         <Text color="white">Mitogenomes</Text>
-        <Text color="white" fw={700} fz={30}>{stats.mitogenomes}</Text>
+        <Text color="white" fw={700} fz={30}>
+          {stats.mitogenomes}
+        </Text>
       </Group>
       <Group position="apart">
         <Text color="white">Barcodes</Text>
-        <Text color="white" fw={700} fz={30}>{stats.barcodes}</Text>
+        <Text color="white" fw={700} fz={30}>
+          {stats.barcodes}
+        </Text>
       </Group>
       <Group position="apart">
         <Text color="white">Other Data</Text>
-        <Text color="white" fw={700} fz={30}>{stats.total - stats.wholeGenomes - stats.mitogenomes - stats.barcodes}</Text>
+        <Text color="white" fw={700} fz={30}>
+          {stats.total -
+            stats.wholeGenomes -
+            stats.mitogenomes -
+            stats.barcodes}
+        </Text>
       </Group>
     </Paper>
-  )
+  );
 }
 
-function Attribution({ name, url }: { name: string, url: string }) {
+function Attribution({ name, url }: { name: string; url: string }) {
   return (
     <Group position="right" mt={20}>
       <Text color="dimmed">Source:</Text>
       <Link href={url}>{name}</Link>
     </Group>
-  )
+  );
 }
 
-function Taxonomy({ taxonomy, regions }: { taxonomy: Taxonomy, regions: Regions }) {
-  const attribution = 'Australian Faunal Directory'
-  const sourceUrl = `https://biodiversity.org.au/afd/taxa/${taxonomy.canonicalName}`
+function Taxonomy({
+  taxonomy,
+  regions,
+}: {
+  taxonomy: Taxonomy;
+  regions: Regions;
+}) {
+  const attribution = "Australian Faunal Directory";
+  const sourceUrl = `https://biodiversity.org.au/afd/taxa/${taxonomy.canonicalName}`;
   const allRegions = [...regions.ibra, ...regions.imcra];
 
   return (
-    <Paper bg="midnight.6" p={40} radius={35}>
+    <Paper bg="midnight.6" p="lg" radius="lg">
       <Grid>
         <Grid.Col span={3}>
-          <Text style={{ paddingBottom: 25 }} color="white">Taxonomy</Text>
+          <Text size="xl" weight={600} color="white">
+            Taxonomy
+          </Text>
           <Stack>
-            <Text color="white" c="dimmed">{taxonomy.kingdom}</Text>
-            <Text color="white" c="dimmed">{taxonomy.phylum}</Text>
-            <Text color="white" c="dimmed">{taxonomy.class}</Text>
-            <Text color="white" c="dimmed">{taxonomy.order}</Text>
+            <Text color="white" c="dimmed">
+              {taxonomy.kingdom}
+            </Text>
+            <Text color="white" c="dimmed">
+              {taxonomy.phylum}
+            </Text>
+            <Text color="white" c="dimmed">
+              {taxonomy.class}
+            </Text>
+            <Text color="white" c="dimmed">
+              {taxonomy.order}
+            </Text>
             <Link href={`/family/${taxonomy.family}`}>{taxonomy.family}</Link>
             <Link href={`/genus/${taxonomy.genus}`}>{taxonomy.genus}</Link>
           </Stack>
         </Grid.Col>
 
         <Grid.Col span="auto">
-          <RegionMap regions={allRegions.map(region => region.name)}/>
+          <RegionMap regions={allRegions.map((region) => region.name)} />
           <Text c="dimmed" fz="sm">
-            {allRegions.map(region => region.name).join(", ")}
+            {allRegions.map((region) => region.name).join(", ")}
           </Text>
 
-          { allRegions.length > 0 ? <Attribution name={attribution} url={sourceUrl} /> : null }
+          {allRegions.length > 0 ? (
+            <Attribution name={attribution} url={sourceUrl} />
+          ) : null}
         </Grid.Col>
       </Grid>
     </Paper>
-  )
+  );
 }
-
 
 export function Summary({ canonicalName }: { canonicalName: string }) {
   const { loading, error, data } = useQuery<QueryResults>(GET_SUMMARY, {
@@ -149,33 +196,45 @@ export function Summary({ canonicalName }: { canonicalName: string }) {
   });
 
   if (error) {
-    return (<Text>Error : {error.message}</Text>);
+    return <Text>Error : {error.message}</Text>;
   }
-
 
   return (
     <>
       <LoadingOverlay
         overlayColor="black"
         transitionDuration={500}
-        loaderProps={{ variant: "bars", size: 'xl', color: "moss.5" }}
+        loaderProps={{ variant: "bars", size: "xl", color: "moss.5" }}
         visible={loading}
       />
 
       <Grid py={40}>
         <Grid.Col span="content">
           <Stack>
-          { data?.species.photos[0]
-            ? <SpeciesPhoto photo={data?.species.photos[0]}/>
-            : <Image width={300} height={300} radius="lg" alt="Species image" withPlaceholder/>
-          }
+            {data?.species.photos[0] ? (
+              <SpeciesPhoto photo={data?.species.photos[0]} />
+            ) : (
+              <Image
+                width={300}
+                height={300}
+                radius="lg"
+                alt="Species image"
+                withPlaceholder
+              />
+            )}
 
-          { data?.stats ? <DataSummary stats={data.stats.species} /> : null }
+            {data?.stats ? <DataSummary stats={data.stats.species} /> : null}
           </Stack>
         </Grid.Col>
         <Grid.Col span="auto">
-          { data ? <Taxonomy taxonomy={data.species.taxonomy} regions={data.species.regions}/> : null }
+          {data ? (
+            <Taxonomy
+              taxonomy={data.species.taxonomy}
+              regions={data.species.regions}
+            />
+          ) : null}
         </Grid.Col>
       </Grid>
-    </>);
+    </>
+  );
 }
--- src/app/species/[name]/layout.tsx
+++ src/app/species/[name]/layout.tsx
@@ -1,83 +1,104 @@
-'use client';
+"use client";
 
 import { gql, useQuery } from "@apollo/client";
-import { Box, Grid, Group, LoadingOverlay, Stack, Text, Title } from "@mantine/core";
+import {
+  ActionIcon,
+  Anchor,
+  Box,
+  Button,
+  Divider,
+  Grid,
+  Group,
+  LoadingOverlay,
+  Stack,
+  Text,
+  Title,
+  Tooltip,
+} from "@mantine/core";
 import { Conservation, Taxonomy } from "@/app/type";
 import IconBar from "../../components/icon-bar";
-
+import { ExternalLink } from "tabler-icons-react";
 
 const GET_SPECIES = gql`
-query SpeciesWithConservation($canonicalName: String) {
-  species(canonicalName: $canonicalName) {
-    taxonomy {
-      canonicalName
-      authorship
-      kingdom
-      phylum
-      class
-      order
-      family
-      genus
-      vernacularGroup
-    }
-    conservation {
-      status
-      state
-      source
+  query SpeciesWithConservation($canonicalName: String) {
+    species(canonicalName: $canonicalName) {
+      taxonomy {
+        canonicalName
+        authorship
+        kingdom
+        phylum
+        class
+        order
+        family
+        genus
+        vernacularGroup
+      }
+      conservation {
+        status
+        state
+        source
+      }
     }
   }
-}`;
+`;
 
 type QueryResults = {
   species: {
-    taxonomy: Taxonomy,
-    conservation: Conservation[],
-  },
+    taxonomy: Taxonomy;
+    conservation: Conservation[];
+  };
 };
 
-
 interface HeaderProps {
-  taxonomy: Taxonomy,
-  conservation?: Conservation[],
+  taxonomy: Taxonomy;
+  conservation?: Conservation[];
 }
 
 function Header({ taxonomy, conservation }: HeaderProps) {
   return (
-    <Stack>
-      <Grid>
-        <Grid.Col span="auto">
-          <Title order={3} color="white" mb={10}>
-            <Group>
-              <Text italic={true} fw={500}>{taxonomy.canonicalName}</Text><Text fw="normal">{taxonomy.authorship}</Text>
-            </Group>
+    <Grid>
+      <Grid.Col span="auto">
+        <Stack justify="center" h="100%" spacing={0} pt="sm">
+          <Title order={3} color="white" size={26}>
+            <i>{taxonomy.canonicalName}</i> {taxonomy.authorship}
           </Title>
-        </Grid.Col>
-        <Grid.Col span="content">
-          <IconBar taxonomy={taxonomy} conservation={conservation} />
-        </Grid.Col>
-      </Grid>
-    </Stack>
-  )
+          <Box mt={4}>
+            <Button
+              radius="md"
+              color="shellfish"
+              size="xs"
+              leftIcon={<ExternalLink size="1rem" />}
+            >
+              View on ALA
+            </Button>
+          </Box>
+        </Stack>
+      </Grid.Col>
+      <Grid.Col span="content">
+        <IconBar taxonomy={taxonomy} conservation={conservation} />
+      </Grid.Col>
+    </Grid>
+  );
 }
-
 
 interface SpeciesLayoutProps {
-  params: { name: string },
-  children: React.ReactNode,
+  params: { name: string };
+  children: React.ReactNode;
 }
 
-export default function SpeciesLayout(props: SpeciesLayoutProps) {
-  const canonicalName = props.params.name.replaceAll("_", " ");
+export default function SpeciesLayout({
+  params,
+  children,
+}: SpeciesLayoutProps) {
+  const canonicalName = params.name.replaceAll("_", " ");
 
   const { loading, error, data } = useQuery<QueryResults>(GET_SPECIES, {
     variables: {
-        canonicalName,
+      canonicalName,
     },
   });
 
-  if (error) {
-    return (<Text>Error : {error.message}</Text>);
-  }
+  if (error) return <Text>Error : {error.message}</Text>;
 
   const taxonomy = data?.species.taxonomy;
   const conservation = data?.species.conservation;
@@ -87,11 +108,11 @@ export default function SpeciesLayout(props: SpeciesLayoutProps) {
       <LoadingOverlay
         overlayColor="black"
         transitionDuration={500}
-        loaderProps={{ variant: "bars", size: 'xl', color: "moss.5" }}
+        loaderProps={{ variant: "bars", size: "xl", color: "moss.5" }}
         visible={loading}
       />
-      { taxonomy ? <Header taxonomy={taxonomy} conservation={conservation} /> : null }
-      { props.children }
+      {taxonomy && <Header taxonomy={taxonomy} conservation={conservation} />}
+      {children}
     </Box>
-  )
+  );
 }
