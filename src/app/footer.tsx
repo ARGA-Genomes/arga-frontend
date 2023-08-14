a916a15a9c9e31626eb14be7c8bb5e96d08d0a2d
Vikas Nagaraju <vikas.nagaraju@csiro.au>
2023-08-14 15:30:28 +1000

    ARGA-281 Footer parameterized and image minimum widths set

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Link from 'next/link';
-import {Container, Divider, Flex, Grid, Image, MediaQuery, Stack, Text} from '@mantine/core';
+import {Container, Divider, Flex, Grid, Image, MediaQuery, Stack, Text, createStyles} from '@mantine/core';
 import { HeaderAndFooterProps } from './type';
 
 
@@ -10,6 +10,12 @@ export function Footer({ links }: HeaderAndFooterProps) {
   const footerLinks = links.map((link) => (
     <Link key={link.label} href='/'> {link.label} </Link>
   ));
+
+  const maxWidth: string = "200px"
+  const minWidth: string = "200px"
+  const mawImage = {
+    base: "50%", xs:maxWidth, sm:maxWidth, md:maxWidth, lg:maxWidth, xl:"100%"
+  }
 
   return (
     <Flex py={50} align="center">
@@ -27,23 +33,23 @@ export function Footer({ links }: HeaderAndFooterProps) {
         <Grid.Col xs={12} sm={12} md={9} lg={9} xl={9}>
           <Container size="xl">
             <Grid gutter={100}>
-            <Grid.Col xs={12} sm={12} md={6} lg={4} xl={4}>
-              <Image src="/ala-logo.svg" maw={{base: "50%", xs:"30%", sm:"30%", md:"50%", lg:"100%", xl:"100%"}} fit="scale-down" alt="" />
+            <Grid.Col xs={12} sm={12} md={6} lg={6} xl={4}>
+              <Image src="/ala-logo.svg" maw={mawImage} miw={minWidth} fit="scale-down" alt="" />
             </Grid.Col>
 
-            <Grid.Col xs={12} sm={12} md={6} lg={4} xl={4}>
-              <Image src="/biocommons-logo.svg" maw={{base: "50%", xs:"30%", sm:"30%", md:"50%", lg:"100%", xl:"100%"}} fit="scale-down" alt="" />
+            <Grid.Col xs={12} sm={12} md={6} lg={6} xl={4}>
+              <Image src="/biocommons-logo.svg" maw={mawImage} miw={minWidth} fit="scale-down" alt="" />
             </Grid.Col>
 
-            <Grid.Col xs={12} sm={12} md={6} lg={4} xl={4}>
-              <Image src="/bioplatforms-logo.svg" maw={{base: "50%", xs:"30%", sm:"30%", md:"50%", lg:"100%", xl:"100%"}} fit="scale-down" alt="" />
+            <Grid.Col xs={12} sm={12} md={6} lg={6} xl={4}>
+              <Image src="/bioplatforms-logo.svg" maw={mawImage} miw={minWidth} fit="scale-down" alt="" />
             </Grid.Col>
 
-            <Grid.Col xs={12} sm={12} md={6} lg={4} xl={4}>
-              <Image src="/ardc-logo.svg" maw={{base: "50%", xs:"30%", sm:"30%", md:"50%", lg:"100%", xl:"100%"}} fit="scale-down" alt="" />
+            <Grid.Col xs={12} sm={12} md={6} lg={6} xl={4}>
+              <Image src="/ardc-logo.svg" maw={mawImage} miw={minWidth} fit="scale-down" alt="" />
             </Grid.Col>
 
-            <Grid.Col xs={12} sm={12} md={6} lg={4} xl={4}>
+            <Grid.Col xs={12} sm={12} md={6} lg={6} xl={4}>
               <Container size={570}>
                 <Text color="white" size="xs">
                   The Australian Reference Genome Atlas (ARGA) is powered by the Atlas of Living Australia,
@@ -55,8 +61,8 @@ export function Footer({ links }: HeaderAndFooterProps) {
               </Container>
             </Grid.Col>
 
-            <Grid.Col xs={12} sm={12} md={6} lg={4} xl={4}>
-              <Image src="/ncris-logo.svg" maw={{base: "50%", xs:"30%", sm:"30%", md:"50%", lg:"100%", xl:"100%"}} fit="scale-down" alt="" />
+            <Grid.Col xs={12} sm={12} md={6} lg={6} xl={4}>
+              <Image src="/ncris-logo.svg" maw={mawImage} miw={minWidth} fit="scale-down" alt="" />
             </Grid.Col>
           </Grid>
           </Container>
