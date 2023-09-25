d07cb52c6ffe8402ac62b14f3e377b3ac5cefa79
Goran Sterjov <goran.sterjov@csiro.au>
2023-09-26 02:08:50 +1000

    removed non-kingdom options from home page

--- src/app/(home)/browse-taxon.tsx
+++ src/app/(home)/browse-taxon.tsx
@@ -78,14 +78,6 @@ export default function BrowseTaxon() {
         <Carousel.Slide>
         <BrowseCard
           total={0}
-          category="Cnidaria"
-          image="card-icons/taxon_Anemones_coralsandjellyfishes(Cnidaria).svg"
-          link="/phylum/Cnidaria"
-        />
-        </Carousel.Slide>
-        <Carousel.Slide>
-        <BrowseCard
-          total={0}
           category="Bacteria"
           image="card-icons/taxon_Bacteria.svg"
           link="kingdom/Bacteria"
@@ -94,121 +86,9 @@ export default function BrowseTaxon() {
         <Carousel.Slide>
         <BrowseCard
           total={0}
-          category="Birds"
-          image="card-icons/taxon_Birds(Aves).svg"
-          link="/class/Aves"
-        />
-        </Carousel.Slide>
-        <Carousel.Slide>
-        <BrowseCard
-          total={0}
-          category="Phaeophyceae"
-          image="card-icons/taxon_Brownalgae(Phaeophyceae).svg"
-          link="/phylum/Phaeophyceae"
-        />
-        </Carousel.Slide>
-        <Carousel.Slide>
-        <BrowseCard
-          total={0}
-          category="Crustacea"
-          image="card-icons/taxon_Crustaceans(Crustacea).svg"
-          link=""
-        />
-        </Carousel.Slide>
-        <Carousel.Slide>
-        <BrowseCard
-          total={0}
-          category="Cyanobacteria"
-          image="card-icons/taxon_Cyanobacteria.svg"
-          link="/phylum/Cyanobacteria"
-        />
-        </Carousel.Slide>
-        <Carousel.Slide>
-        <BrowseCard
-          total={0}
-          category="Echinodermata"
-          image="card-icons/taxon_Echinoderms(Echinodermata).svg"
-          link="/phylum/Echinodermata"
-        />
-        </Carousel.Slide>
-        <Carousel.Slide>
-        <BrowseCard
-          total={0}
-          category="Actinopterygii"
-          image="card-icons/taxon_Finfishes(Actinopterygii).svg"
-          link="/class/Actinopterygii"
-        />
-        </Carousel.Slide>
-        <Carousel.Slide>
-        <BrowseCard
-          total={0}
-          category="Anura"
-          image="card-icons/taxon_Frogsandtoads(Anura).svg"
-          link="/order/Anura"
-        />
-        </Carousel.Slide>
-        <Carousel.Slide>
-        <BrowseCard
-          total={0}
-          category="Chlorophyta"
-          image="card-icons/taxon_Greenalgae(Chlorophyta).svg"
-          link="/phylum/Chlorophyta"
-        />
-        </Carousel.Slide>
-        <Carousel.Slide>
-        <BrowseCard
-          total={0}
-          category="Insecta"
-          image="card-icons/taxon_Insects(Insecta).svg"
-          link="/class/Insecta"
-        />
-        </Carousel.Slide>
-        <Carousel.Slide>
-        <BrowseCard
-          total={0}
-          category="Mammalia"
-          image="card-icons/taxon_Mammals(Mammalia).svg"
-          link="/class/Mammalia"
-        />
-        </Carousel.Slide>
-        <Carousel.Slide>
-        <BrowseCard
-          total={0}
-          category="Mollusca"
-          image="card-icons/taxon_Molluscs(Mollusca).svg"
-          link="/phylum/Mollusca"
-        />
-        </Carousel.Slide>
-        <Carousel.Slide>
-        <BrowseCard
-          total={0}
           category="Protista"
           image="card-icons/taxon_Protists(Protista).svg"
           link="/kingdom/Protista"
-        />
-        </Carousel.Slide>
-        <Carousel.Slide>
-        <BrowseCard
-          total={0}
-          category="Rhodophyta"
-          image="card-icons/taxon_Redalgae(Rhodophyta).svg"
-          link="/phylum/Rhodophyta"
-        />
-        </Carousel.Slide>
-        <Carousel.Slide>
-        <BrowseCard
-          total={0}
-          category="Elasmobranchii"
-          image="card-icons/taxon_Sharksandrays(Elasmobranchii).svg"
-          link=""
-        />
-        </Carousel.Slide>
-        <Carousel.Slide>
-        <BrowseCard
-          total={0}
-          category="Porifera"
-          image="card-icons/taxon_Sponges(Porifera).svg"
-          link="/phylum/Porifera"
         />
         </Carousel.Slide>
     </Carousel>
