f42644cb6d6f7de730c06586e33a1ef49dbd689d
Goran Sterjov <goran.sterjov@csiro.au>
2023-10-02 15:21:43 +1100

    use species component

--- src/app/(taxonomy)/[rank]/[name]/page.tsx
+++ src/app/(taxonomy)/[rank]/[name]/page.tsx
@@ -7,11 +7,9 @@ import {
   Title,
   Box,
   Text,
-  Card,
   SimpleGrid,
   Stack,
   Grid,
-  Image,
   Container,
   Table,
   Group,
@@ -23,8 +21,7 @@ import {
   Avatar,
 } from "@mantine/core";
 
-import Link from "next/link";
-import { CircleCheck, CircleX, Filter as IconFilter, SortAscending } from "tabler-icons-react";
+import { Filter as IconFilter, SortAscending } from "tabler-icons-react";
 import { useEffect, useState } from "react";
 import { LoadOverlay, LoadPanel } from "@/components/load-overlay";
 import { Filter, intoFilterItem } from "@/components/filtering/common";
@@ -45,6 +42,7 @@ import { PieChart } from "@/components/graphing/pie";
 import { BarChart } from "@/components/graphing/bar";
 import { BushfireRecoveryFilters } from '@/components/filtering/bushfire-recovery';
 import { DataTable, DataTableRow } from '@/components/data-table';
+import { SpeciesCard } from '@/components/species-card';
 
 
 const PAGE_SIZE = 10;
@@ -185,57 +183,6 @@ type QueryResults = {
 
 type TaxonResults = {
   taxon: Taxonomy,
-}
-
-
-function DataItem({ name, count }: { name: string; count: number }) {
-  const hasData = count > 0;
-
-  return (
-    <Group>
-      {hasData ? <CircleCheck color="green" /> : <CircleX color="red" />}
-      <Text fw={300} fz="xs">{ name }</Text>
-    </Group>
-  );
-}
-
-function SpeciesCard({ species }: { species: Species }) {
-  const itemLinkName = species.taxonomy.canonicalName?.replaceAll(" ", "_");
-
-  function small(photo: { url: string }) {
-    return photo.url.replaceAll("original", "small");
-  }
-
-  return (
-    <Card shadow="sm" p={20} radius="lg" withBorder>
-      <Card.Section>
-        <Link href={`/species/${itemLinkName}/taxonomy`}>
-          {species.photo ? (
-            <Image
-              src={small(species.photo)}
-              height={260}
-              alt={species.taxonomy.canonicalName}
-            />
-          ) : (
-            <Image
-              height={260}
-              alt={species.taxonomy.canonicalName}
-            />
-          )}
-        </Link>
-      </Card.Section>
-
-      <Stack gap={5}>
-        <Link href={`/species/${itemLinkName}/taxonomy`}>
-          <Text fz="sm" fw={700} fs="italic">{species.taxonomy.canonicalName}</Text>
-        </Link>
-        <SimpleGrid cols={2}>
-          <DataItem name="Genome" count={species.dataSummary.wholeGenomes} />
-          <DataItem name="Genetic marker" count={species.dataSummary.barcodes} />
-        </SimpleGrid>
-      </Stack>
-    </Card>
-  );
 }
 
 
