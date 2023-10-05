3f684828da286d810fe05194c5f084f65c4f1e86
Goran Sterjov <goran.sterjov@csiro.au>
2023-10-05 11:22:58 +1100

    added imcra regions on new map

--- src/components/mapping/analysis-map.tsx
+++ src/components/mapping/analysis-map.tsx
@@ -1,78 +1,14 @@
 "use client";
 
 import Map from 'react-map-gl/maplibre';
-import DeckGL, { BitmapLayer, GeoJsonLayer, TileLayer } from 'deck.gl/typed';
+import DeckGL, { BitmapLayer, GeoJsonLayer, PathLayer, TileLayer } from 'deck.gl/typed';
 import { useEffect, useState } from 'react';
+import { GeoJSON } from 'geojson';
 import { gql, useQuery } from '@apollo/client';
 
 
 // center on Australia by default
 const DEFAULT_POSITION = [-28.30638, 134.3838];
-
-
-interface Regions {
-  ibra: string[],
-  imcra: string[],
-}
-
-
-interface AnalysisMapProps {
-  regions?: Regions,
-  children?: React.ReactNode,
-}
-
-export default function AnalysisMap({ regions, children }: AnalysisMapProps) {
-  let layers: any[] = [tileLayer()];
-
-  if (regions) {
-    const allRegions = [...regions.ibra, ...regions.imcra];
-    layers.push(bioRegionAnalysisLayers({ regions: allRegions }));
-  }
-
-  return (
-    <DeckGL
-      initialViewState={{
-        latitude: DEFAULT_POSITION[0],
-        longitude: DEFAULT_POSITION[1],
-        zoom: 4,
-      }}
-      layers={layers}
-      controller={true}
-      style={{ width: '100%', height: '100%' }}
-    >
-      <Map />
-      {children}
-    </DeckGL>
-  )
-}
-
-/* mapStyle='https://basemaps.cartocdn.com/gl/positron-gl-style/style.json' */
-
-
-function tileLayer() {
-  return new TileLayer({
-    // https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#Tile_servers
-    data: 'https://c.tile.openstreetmap.org/{z}/{x}/{y}.png',
-
-    minZoom: 0,
-    maxZoom: 19,
-    tileSize: 256,
-
-
-          renderSubLayers: props => {
-            const {
-              bbox: {west, south, east, north}
-            } = props.tile;
-
-            return new BitmapLayer(props, {
-              data: null,
-              image: props.data,
-              bounds: [west, south, east, north]
-            });
-          }
-  });
-}
-
 
 const GET_GEOMETRY = gql`
   query BioRegions($regions: [String], $tolerance: Float) {
@@ -93,32 +29,102 @@ type QueryResults = {
 };
 
 
-function bioRegionAnalysisLayers({ regions }: { regions: string[] }) {
-  const [ibra, setIbra] = useState(undefined);
+interface Regions {
+  ibra: string[],
+  imcra: string[],
+}
+
+
+interface AnalysisMapProps {
+  regions?: Regions,
+  children?: React.ReactNode,
+}
+
+export default function AnalysisMap({ regions, children }: AnalysisMapProps) {
   const [tolerance, setTolerance] = useState(0.01);
 
   const { loading, error, data } = useQuery<QueryResults>(GET_GEOMETRY, {
     variables: {
-      regions: regions,
+      regions: regions ? [...regions.ibra, ...regions.imcra] : [],
       tolerance: tolerance,
     },
   });
 
-  useEffect(() => {
-    if (data) {
-      setIbra(JSON.parse(data.maps.ibra));
-    }
-  }, [data, setIbra]);
+  let bioRegions = {
+    ibra: data && JSON.parse(data.maps.ibra) as GeoJSON,
+    imcra: data && JSON.parse(data.maps.imcraProvincial) as GeoJSON,
+  }
 
-  return new GeoJsonLayer({
+  return (
+    <DeckGL
+      initialViewState={{
+        latitude: DEFAULT_POSITION[0],
+        longitude: DEFAULT_POSITION[1],
+        zoom: 4,
+      }}
+      layers={[
+        tileLayer(),
+        bioRegionLayers(bioRegions),
+      ]}
+      controller={true}
+      style={{ width: '100%', height: '100%' }}
+    >
+      <Map />
+      {children}
+    </DeckGL>
+  )
+}
+
+
+function tileLayer() {
+  return new TileLayer({
+    data: 'https://c.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    maxRequests: 20,
+    pickable: true,
+    autoHighlight: false,
+    highlightColor: [60, 60, 60, 40],
+    minZoom: 0,
+    maxZoom: 19,
+    tileSize: 256,
+    renderSubLayers: props => {
+      const [[west, south], [east, north]] = props.tile.boundingBox;
+      return new BitmapLayer(props, {
+        data: undefined,
+        image: props.data,
+        bounds: [west, south, east, north]
+      })
+    },
+  });
+}
+
+
+interface BioRegions {
+  ibra?: GeoJSON,
+  imcra?: GeoJSON,
+}
+
+function bioRegionLayers(regions: BioRegions) {
+  const ibra = new GeoJsonLayer({
     id: "ibra-layer",
-    data: ibra || [],
-    getFillColor: [218, 93, 11, 30],
-    getLineColor: [218, 93, 11, 200],
-
+    data: regions.ibra || [],
+    getFillColor: [244, 124, 46, 70],
+    getLineColor: [244, 124, 46, 200],
     pickable: true,
     filled: true,
     pointType: 'circle',
     lineWidthMinPixels: 1,
-  })
+  });
+
+  const imcra = new GeoJsonLayer({
+    id: "imcra-layer",
+    data: regions.imcra || [],
+    getFillColor: [244, 124, 46, 70],
+    getLineColor: [244, 124, 46, 200],
+    pickable: true,
+    filled: true,
+    pointType: 'circle',
+    lineWidthMinPixels: 1,
+  });
+
+  return [ibra, imcra];
 }
