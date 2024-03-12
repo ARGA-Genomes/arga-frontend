81f1a2f70650a5d751c577c267ebb8e2b1099d77
Caitlin <crams27@gmail.com>
2024-03-12 11:48:52 +1000

    changed ui to make species header more responsive when resizing window - ie. changed the header from a grid to flex components

--- src/components/species-header.tsx
+++ src/components/species-header.tsx
@@ -1,97 +1,197 @@
-'use client';
+"use client";
 
 import { gql, useQuery } from "@apollo/client";
-import { Container, Grid, Group, Paper, Text, useMantineTheme } from "@mantine/core";
-import { Conservation, IndigenousEcologicalKnowledge, Taxonomy } from "@/app/type";
+import {
+  Container,
+  Grid,
+  Group,
+  Paper,
+  Text,
+  Flex,
+  useMantineTheme,
+} from "@mantine/core";
+import {
+  Conservation,
+  IndigenousEcologicalKnowledge,
+  Taxonomy,
+} from "@/app/type";
 import IconBar from "./icon-bar";
 import { LoadOverlay } from "./load-overlay";
 import { MAX_WIDTH } from "@/app/constants";
 import { CircleCheck, CircleX } from "tabler-icons-react";
 
-
 const GET_SPECIES = gql`
-query SpeciesWithConservation($canonicalName: String) {
-  species(canonicalName: $canonicalName) {
-    taxonomy {
-      canonicalName
-      vernacularGroup
-    }
-    conservation {
-      status
-      state
-      source
-    }
-    indigenousEcologicalKnowledge {
-      id
-      name
-      datasetName
-      culturalConnection
-      foodUse
-      medicinalUse
-      sourceUrl
-    }
-    referenceGenome {
-      recordId
+  query SpeciesWithConservation($canonicalName: String) {
+    species(canonicalName: $canonicalName) {
+      taxonomy {
+        canonicalName
+        vernacularGroup
+      }
+      conservation {
+        status
+        state
+        source
+      }
+      indigenousEcologicalKnowledge {
+        id
+        name
+        datasetName
+        culturalConnection
+        foodUse
+        medicinalUse
+        sourceUrl
+      }
+      referenceGenome {
+        recordId
+      }
     }
   }
-}`;
+`;
 
 type QueryResults = {
   species: {
-    taxonomy: Taxonomy[],
-    conservation: Conservation[],
-    indigenousEcologicalKnowledge: IndigenousEcologicalKnowledge[],
-    referenceGenome?: { recordId: string }
-  },
+    taxonomy: Taxonomy[];
+    conservation: Conservation[];
+    indigenousEcologicalKnowledge: IndigenousEcologicalKnowledge[];
+    referenceGenome?: { recordId: string };
+  };
 };
 
-
 interface HeaderProps {
-  taxonomy: Taxonomy[],
-  conservation?: Conservation[],
-  traits?: IndigenousEcologicalKnowledge[],
-  referenceGenome?: { recordId: string },
+  taxonomy: Taxonomy[];
+  conservation?: Conservation[];
+  traits?: IndigenousEcologicalKnowledge[];
+  referenceGenome?: { recordId: string };
 }
 
-function Header({ taxonomy, conservation, traits, referenceGenome }: HeaderProps) {
+function Header({
+  taxonomy,
+  conservation,
+  traits,
+  referenceGenome,
+}: HeaderProps) {
   const theme = useMantineTheme();
 
   return (
-    <Grid>
-      <Grid.Col span="auto">
-        <Group gap={40}>
-          <Text c="dimmed" fw={400}>Species</Text>
-          <Text fz={38} fw={700} fs="italic">{taxonomy[0]?.canonicalName}</Text>
+    <Flex
+      justify="space-between"
+      direction={{ base: "column", md: "row" }}
+      gap="md"
+      wrap="wrap"
+    >
+      <Flex
+        direction={{ base: "column", md: "row" }}
+        gap={{ base: 0, md: 40 }}
+        align={{ base: "normal", md: "center" }}
+      >
+        <Text c="dimmed" fw={400}>
+          Species
+        </Text>
+        <Text fz={38} fw={700} fs="italic">
+          {taxonomy[0]?.canonicalName}
+        </Text>
+      </Flex>
+
+      <Flex
+        direction={{ base: "column", md: "row" }}
+        gap={{ base: "md", md: "xl" }}
+      >
+        <IconBar
+          taxonomy={taxonomy[0]}
+          conservation={conservation}
+          traits={traits}
+        />
+
+        <Group h="100%" wrap="nowrap">
+          <Text fw={700} c="dimmed" style={{ whiteSpace: "nowrap" }}>
+            Reference Genome
+          </Text>
+          {referenceGenome ? (
+            <CircleCheck size={35} color={theme.colors.moss[5]} />
+          ) : (
+            <CircleX size={35} color={theme.colors.red[5]} />
+          )}
         </Group>
-      </Grid.Col>
-      <Grid.Col span="content">
-        <IconBar taxonomy={taxonomy[0]} conservation={conservation} traits={traits} />
-      </Grid.Col>
-      <Grid.Col span="content" ml={80}>
-        <Group h="100%" pl={30}>
-          <Text fw={700} c="dimmed">Reference Genome</Text>
-          { referenceGenome
-            ? <CircleCheck size={35} color={theme.colors.moss[5]} />
-            : <CircleX size={35} color={theme.colors.red[5]} />
-          }
-        </Group>
-      </Grid.Col>
-    </Grid>
-  )
+      </Flex>
+    </Flex>
+    // <Flex justify="space-between" direction={{ base: "column", md: "row" }}>
+    //   <Group gap={40}>
+    //     <Text c="dimmed" fw={400}>
+    //       Species
+    //     </Text>
+    //     <Text fz={38} fw={700} fs="italic">
+    //       {taxonomy[0]?.canonicalName}
+    //     </Text>
+    //   </Group>
+
+    //   <Flex>
+    //     <IconBar
+    //       taxonomy={taxonomy[0]}
+    //       conservation={conservation}
+    //       traits={traits}
+    //     />
+
+    //     <Group h="100%">
+    //       <Text fw={700} c="dimmed">
+    //         Reference Genome
+    //       </Text>
+    //       {referenceGenome ? (
+    //         <CircleCheck size={35} color={theme.colors.moss[5]} />
+    //       ) : (
+    //         <CircleX size={35} color={theme.colors.red[5]} />
+    //       )}
+    //     </Group>
+    //   </Flex>
+    // </Flex>
+    // <Grid>
+    //   <Grid.Col span="auto">
+    //     <Group gap={40}>
+    //       <Text c="dimmed" fw={400}>
+    //         Species
+    //       </Text>
+    //       <Text fz={38} fw={700} fs="italic">
+    //         {taxonomy[0]?.canonicalName}
+    //       </Text>
+    //     </Group>
+    //   </Grid.Col>
+    //   <Grid.Col span="content">
+    //     <IconBar
+    //       taxonomy={taxonomy[0]}
+    //       conservation={conservation}
+    //       traits={traits}
+    //     />
+    //   </Grid.Col>
+    //   <Grid.Col span="content" ml={80}>
+    //     <Group h="100%" pl={30}>
+    //       <Text fw={700} c="dimmed">
+    //         Reference Genome
+    //       </Text>
+    //       {referenceGenome ? (
+    //         <CircleCheck size={35} color={theme.colors.moss[5]} />
+    //       ) : (
+    //         <CircleX size={35} color={theme.colors.red[5]} />
+    //       )}
+    //     </Group>
+    //   </Grid.Col>
+    // </Grid>
+  );
 }
 
-
-export default function SpeciesHeader({ canonicalName }: { canonicalName: string }) {
+export default function SpeciesHeader({
+  canonicalName,
+}: {
+  canonicalName: string;
+}) {
   const { loading, error, data } = useQuery<QueryResults>(GET_SPECIES, {
     variables: {
-        canonicalName,
+      canonicalName,
     },
   });
 
   if (error) {
-    return (<Text>Error : {error.message}</Text>);
+    return <Text>Error : {error.message}</Text>;
   }
-  
+
   const taxonomy = data?.species.taxonomy;
   const conservation = data?.species.conservation;
   const traits = data?.species.indigenousEcologicalKnowledge;
@@ -101,15 +201,15 @@ export default function SpeciesHeader({ canonicalName }: { canonicalName: string
     <Paper py={20} pos="relative">
       <LoadOverlay visible={loading} />
       <Container maw={MAX_WIDTH}>
-      { taxonomy
-        ? <Header
+        {taxonomy ? (
+          <Header
             taxonomy={taxonomy}
             conservation={conservation}
             traits={traits}
             referenceGenome={referenceGenome}
           />
-        : null }
+        ) : null}
       </Container>
     </Paper>
-  )
+  );
 }
