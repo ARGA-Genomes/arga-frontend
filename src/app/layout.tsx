e2b0607a82eb05d3af2a855ac1befb6d5b359b75
Caitlin <crams27@gmail.com>
2024-03-18 10:37:38 +1000

    made app header more responsive and fixed burger drop down menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,42 +1,49 @@
-'use client';
+"use client";
 
-import '@mantine/core/styles.css';
-import './global.css';
-import classes from './layout.module.css';
+import "@mantine/core/styles.css";
+import "./global.css";
+import classes from "./layout.module.css";
 
-import { ApolloProvider } from '@apollo/client';
-import createClient from '../queries/client';
+import { ApolloProvider } from "@apollo/client";
+import createClient from "../queries/client";
 
-import { AppShell, Container, Image, MantineProvider, Paper } from '@mantine/core';
+import {
+  AppShell,
+  Container,
+  Image,
+  MantineProvider,
+  Paper,
+} from "@mantine/core";
 
-import { theme } from '../theme';
-import { TopNav } from './top-nav';
-import { MAX_WIDTH } from './constants';
-import { Footer } from './footer';
-import { TraceLoaderProvider } from '@/components/traces/context';
-import Fathom from '@/components/fathom';
+import { theme } from "../theme";
+import { TopNav } from "./top-nav";
+import { MAX_WIDTH } from "./constants";
+import { Footer } from "./footer";
+import { TraceLoaderProvider } from "@/components/traces/context";
+import Fathom from "@/components/fathom";
 
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   // make the apollo graphql client available on all pages
   const client = createClient();
 
   return (
     <html lang="en">
       <body>
-        <Fathom/>
+        <Fathom />
         <ApolloProvider client={client}>
           <MantineProvider theme={theme}>
             <TraceLoaderProvider>
-            <Shell>
-              {children}
-            </Shell>
+              <Shell>{children}</Shell>
             </TraceLoaderProvider>
           </MantineProvider>
         </ApolloProvider>
       </body>
     </html>
-  )
+  );
 }
 
 // The shell is the visual layout of the site that wraps all content
@@ -50,16 +57,14 @@ function Shell({ children }: { children: React.ReactNode }) {
         <TopNav />
       </AppShell.Header>
 
-      <AppShell.Main>
-        {children}
-      </AppShell.Main>
+      <AppShell.Main>{children}</AppShell.Main>
 
       <Paper className={classes.footer} radius={0}>
         <Container maw={MAX_WIDTH}>
           <Footer />
         </Container>
-          <Image src="/dna_footer.jpg" h={160} />
+        <Image src="/dna_footer.jpg" h={160} />
       </Paper>
     </AppShell>
-  )
+  );
 }
--- src/app/top-nav.tsx
+++ src/app/top-nav.tsx
@@ -1,25 +1,40 @@
-'use client';
+"use client";
 
-import classes from './top-nav.module.css';
+import classes from "./top-nav.module.css";
 
-import Link from 'next/link';
-import { Burger, Transition, NavLink, Group, Image, Stack, Text, Avatar, Indicator, Modal } from '@mantine/core';
-import { useDisclosure, useLocalStorage } from '@mantine/hooks';
-import { useState } from 'react';
-import { Checklist } from 'tabler-icons-react';
-
+import Link from "next/link";
+import {
+  Burger,
+  Transition,
+  NavLink,
+  Group,
+  Image,
+  Stack,
+  Text,
+  Avatar,
+  Indicator,
+  Modal,
+  useMantineTheme,
+  Collapse,
+} from "@mantine/core";
+import { useDisclosure, useLocalStorage } from "@mantine/hooks";
+import { useState } from "react";
+import { Checklist } from "tabler-icons-react";
 
 const links = [
   { link: "/", label: "APP HOME" },
   { link: "/datasets", label: "DATA SOURCES" },
   { link: "https://arga.org.au/", label: "PROJECT HOME" },
-]
-
+];
 
 export function TopNav() {
+  const theme = useMantineTheme();
   const [opened, { toggle, close }] = useDisclosure(false);
   const [active, setActive] = useState(0);
-  const [saved, _setSaved] = useLocalStorage<string[]>({ key: 'save-list', defaultValue: [] });
+  const [saved, _setSaved] = useLocalStorage<string[]>({
+    key: "save-list",
+    defaultValue: [],
+  });
   const [savedOpened, savedHandler] = useDisclosure(false);
 
   const items = links.map((item, index) => (
@@ -44,43 +59,77 @@ export function TopNav() {
 
   const showSaved = () => {
     savedHandler.open();
-  }
+  };
 
   return (
-    <Stack>
+    <Stack gap="0" h="100%">
+      <Modal
+        opened={savedOpened}
+        onClose={savedHandler.close}
+        title="Saved list"
+        size="auto"
+        centered
+      >
+        {saved?.map((item, idx) => (
+          <Link href={item} key={idx}>
+            <Text>{item}</Text>
+          </Link>
+        ))}
+      </Modal>
 
-    <Modal opened={savedOpened} onClose={savedHandler.close} title="Saved list" size="auto" centered>
-      { saved?.map((item, idx) => <Link href={item} key={idx}><Text>{item}</Text></Link>) }
-    </Modal>
+      <Group
+        justify="space-between"
+        align="center"
+        // px={30}
+        wrap="nowrap"
+        h="100%"
+      >
+        <Link href="/" onClick={() => setActive(0)}>
+          <Image
+            // py="50px"
+            p="md"
+            src="/arga-logo.svg"
+            alt="Australian Reference Genome Atlas"
+            width={450}
+          />
+        </Link>
 
-    <Group justify="space-between" align="end" px={30}>
-      <Link href='/' onClick={() => setActive(0)}>
-        <Image py={30} src='/arga-logo.svg' alt='Australian Reference Genome Atlas' width={450} />
-      </Link>
+        <Stack h="100%">
+          <Group align="center" justify="end" mr={20} wrap="nowrap" h="100%">
+            <Indicator
+              inline
+              label={saved?.length}
+              size={16}
+              color="bushfire"
+              disabled={!saved?.length}
+            >
+              <Avatar size="lg" onClick={showSaved} component="a" href="#">
+                <Checklist size={30} color="white" />
+              </Avatar>
+            </Indicator>
+            <Burger
+              hiddenFrom="md"
+              opened={opened}
+              onClick={toggle}
+              size="md"
+              color={"white"}
+            />
+          </Group>
+          <Group visibleFrom="md">{items}</Group>
+        </Stack>
+      </Group>
 
-      <Burger hiddenFrom='md' opened={opened} onClick={toggle} size='md' color={'white'} />
-
-      <Stack>
-        <Group align="center" justify="end" mr={20}>
-          <Indicator inline label={saved?.length} size={16} color="bushfire" disabled={!saved?.length}>
-            <Avatar size="lg" onClick={showSaved} component="a" href="#">
-              <Checklist size={30} color="white" />
-            </Avatar>
-          </Indicator>
-        </Group>
-        <Group visibleFrom="md">
+      <Collapse in={opened} hiddenFrom="md">
+        <Stack h="100vh" bg={theme.colors.midnight[8]}>
           {items}
-        </Group>
-      </Stack>
-    </Group>
+        </Stack>
+      </Collapse>
 
-
-      <Stack hiddenFrom="md">
-        <Transition transition='pop-top-right' duration={200} mounted={opened}>
-          {(_styles) => <Stack>{items}</Stack>}
+      {/* <Stack hiddenFrom="md" bg={theme.colors.midnight[8]}>
+        <Transition transition="pop-top-right" duration={200} mounted={opened}>
+          {(_styles) => <Stack gap="0">{items}</Stack>}
         </Transition>
-      </Stack>
-
+      </Stack> */}
     </Stack>
   );
 }
