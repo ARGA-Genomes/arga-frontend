6253ebdcad071f6736de5aef99f8a8f6d4132a0b
Caitlin <crams27@gmail.com>
2024-03-26 09:20:56 +1000

    restructured homepage to make it more responsive when resizing

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -8,6 +8,9 @@ import {
   Stack,
   TextInput,
   Container,
+  Flex,
+  Box,
+  Center,
 } from "@mantine/core";
 import { useRouter } from "next/navigation";
 import BrowseGrouping from "./browse-grouping";
@@ -17,7 +20,6 @@ import { useState } from "react";
 import BrowseType from "@/app/(home)/browse-type";
 import { MAX_WIDTH } from "../constants";
 import ShowStats from "./stats";
-
 
 function Search() {
   const router = useRouter();
@@ -63,53 +65,139 @@ function Search() {
 
 export default function HomePage() {
   return (
-    <Paper bg="midnight.7" radius={0}>
-      <Paper bg="midnight.6" py={80} px={150} style={{textAlign: 'justify'}}>
-        <Grid>
-          <Grid.Col span={{ base: 12, md: 6, lg: 6, xl: 6 }}>
-            <Stack>
-              <Title order={3} c="wheat.4">
-                deepen discovery - trawl traits - curate collections
+    <Box>
+      <Box bg="midnight.6">
+        <Container maw={MAX_WIDTH}>
+          <Center>
+            <Flex
+              direction={{ base: "column", lg: "row" }}
+              gap={30}
+              justify="space-between"
+              maw={{ base: 650, lg: "100%" }}
+            >
+              <Stack pt={80}>
+                <Title order={3} c="wheat.4">
+                  deepen discovery - trawl traits - curate collections
+                </Title>
+                <Text c="white">
+                  For plants, animals, microbes and other species endemic or
+                  relevant to Australia, the Australian Reference Genome Atlas
+                  (ARGA) locates and aggregates genomic data, including: &nbsp;{" "}
+                  <br></br>
+                  <b>
+                    &#x2022; reference genome assemblies &#x2022; genome
+                    annotations &#x2022; population and variant sets &#x2022;
+                    DNA barcodes &#x2022; coding and non-coding DNA sequences
+                    &#x2022; raw genomics data
+                  </b>
+                </Text>
+                <Text c="white">
+                  Search by species, higher classification, data type or
+                  ecological and phenotypic traits. Get started by entering any
+                  word in the search bar below, or scroll down to browse
+                  pre-filtered groupings.
+                </Text>
+              </Stack>
+              <Box pt={{ base: 0, lg: 40 }} pb={30}>
+                <ShowStats />
+              </Box>
+            </Flex>
+          </Center>
+        </Container>
+      </Box>
+      <Box bg="midnight.7">
+        <Container maw={MAX_WIDTH} py={100}>
+          <Stack gap={80}>
+            <Stack gap={40}>
+              <Title order={3} c="moss.5" fz={30} fw={600}>
+                Search for data
               </Title>
-              <Text c="white">
-                For plants, animals, microbes and other species endemic or relevant to Australia, the Australian Reference
-                Genome Atlas (ARGA) locates and aggregates genomic data, including: &nbsp; <br></br>
-                <b>
-                &#x2022; reference genome assemblies &#x2022; genome annotations &#x2022; population and variant sets &#x2022;
-                  DNA barcodes &#x2022; coding and non-coding DNA sequences &#x2022; raw genomics data
-                </b>
-            </Text>
-            <Text c="white">
-                  Search by species, higher classification, data type or ecological and phenotypic traits. Get started by entering any word in the search bar below, or scroll down to browse pre-filtered groupings.
-              </Text>
+              <Search />
             </Stack>
-          </Grid.Col>
-        </Grid>
-      </Paper>
-      <Paper>
-        <ShowStats />
-      </Paper>
+            <Stack gap={40} pt={40}>
+              <Title order={3} c="moss.5" fz={30} fw={600}>
+                Browse by data type
+              </Title>
+              <BrowseType />
+            </Stack>
+            <Stack gap={40}>
+              <Title order={3} c="moss.5" fz={30} fw={600}>
+                Browse by taxonomic group
+              </Title>
+              <BrowseTaxon />
+            </Stack>
+            <Stack gap={40}>
+              <Title order={3} c="moss.5" fz={30} fw={600}>
+                Browse by functional or ecological group
+              </Title>
+              <BrowseGrouping />
+            </Stack>
+          </Stack>
+        </Container>
+      </Box>
+    </Box>
+    // <Paper bg="midnight.7" radius={0}>
+    //   <Paper bg="midnight.6" py={80} px={150} style={{ textAlign: "justify" }}>
+    //     <Grid>
+    //       <Grid.Col span={{ base: 12, md: 6, lg: 6, xl: 6 }}>
+    //         <Stack>
+    //           <Title order={3} c="wheat.4">
+    //             deepen discovery - trawl traits - curate collections
+    //           </Title>
+    //           <Text c="white">
+    //             For plants, animals, microbes and other species endemic or
+    //             relevant to Australia, the Australian Reference Genome Atlas
+    //             (ARGA) locates and aggregates genomic data, including: &nbsp;{" "}
+    //             <br></br>
+    //             <b>
+    //               &#x2022; reference genome assemblies &#x2022; genome
+    //               annotations &#x2022; population and variant sets &#x2022; DNA
+    //               barcodes &#x2022; coding and non-coding DNA sequences &#x2022;
+    //               raw genomics data
+    //             </b>
+    //           </Text>
+    //           <Text c="white">
+    //             Search by species, higher classification, data type or
+    //             ecological and phenotypic traits. Get started by entering any
+    //             word in the search bar below, or scroll down to browse
+    //             pre-filtered groupings.
+    //           </Text>
+    //         </Stack>
+    //       </Grid.Col>
+    //     </Grid>
+    //   </Paper>
+    //   <Paper>
+    //     <ShowStats />
+    //   </Paper>
 
-      <Container maw={MAX_WIDTH} py={100}>
-        <Stack gap={80}>
-          <Stack gap={40}>
-            <Title order={3} c="moss.5" fz={30} fw={600}>Search for data</Title>   
-            <Search />
-          </Stack>
-          <Stack gap={40} pt={40}>
-            <Title order={3} c="moss.5" fz={30} fw={600}>Browse by data type</Title>
-            <BrowseType />
-          </Stack>
-          <Stack gap={40}>
-            <Title order={3} c="moss.5" fz={30} fw={600}>Browse by taxonomic group</Title>
-            <BrowseTaxon />
-          </Stack>
-          <Stack gap={40}>
-            <Title order={3} c="moss.5" fz={30} fw={600}>Browse by functional or ecological group</Title>
-            <BrowseGrouping />
-          </Stack>
-        </Stack>
-      </Container>
-    </Paper>
+    //   <Container maw={MAX_WIDTH} py={100}>
+    //     <Stack gap={80}>
+    //       <Stack gap={40}>
+    //         <Title order={3} c="moss.5" fz={30} fw={600}>
+    //           Search for data
+    //         </Title>
+    //         <Search />
+    //       </Stack>
+    //       <Stack gap={40} pt={40}>
+    //         <Title order={3} c="moss.5" fz={30} fw={600}>
+    //           Browse by data type
+    //         </Title>
+    //         <BrowseType />
+    //       </Stack>
+    //       <Stack gap={40}>
+    //         <Title order={3} c="moss.5" fz={30} fw={600}>
+    //           Browse by taxonomic group
+    //         </Title>
+    //         <BrowseTaxon />
+    //       </Stack>
+    //       <Stack gap={40}>
+    //         <Title order={3} c="moss.5" fz={30} fw={600}>
+    //           Browse by functional or ecological group
+    //         </Title>
+    //         <BrowseGrouping />
+    //       </Stack>
+    //     </Stack>
+    //   </Container>
+    // </Paper>
   );
 }
--- src/app/(home)/stats.tsx
+++ src/app/(home)/stats.tsx
@@ -5,88 +5,100 @@ import { DataTable, DataTableRow } from "@/components/data-table";
 import { TachoChart } from "@/components/graphing/tacho";
 import { gql, useQuery } from "@apollo/client";
 import { Grid, Paper, Stack, Title, Text } from "@mantine/core";
-import * as Humanize from 'humanize-plus';
+import * as Humanize from "humanize-plus";
 import { BarChart } from "@/components/graphing/bar";
 import { LoadOverlay } from "@/components/load-overlay";
 
 const GET_TAXON = gql`
-query HomeStats {
-  taxon(rank: DOMAIN, canonicalName: "Eukaryota") {
-    summary {
-      species
-      speciesData
-      speciesGenomes
-    }
+  query HomeStats {
+    taxon(rank: DOMAIN, canonicalName: "Eukaryota") {
+      summary {
+        species
+        speciesData
+        speciesGenomes
+      }
 
-    speciesSummary {
-      name
-      genomes
-      totalGenomic
-    }
+      speciesSummary {
+        name
+        genomes
+        totalGenomic
+      }
 
-    speciesGenomeSummary {
-      name
-      genomes
-      totalGenomic
+      speciesGenomeSummary {
+        name
+        genomes
+        totalGenomic
+      }
     }
   }
-}`;
+`;
 
 type DataBreakdown = {
-  name: string,
-  genomes: number,
-  totalGenomic: number,
-}
+  name: string;
+  genomes: number;
+  totalGenomic: number;
+};
 
 type Taxonomy = {
-  dataSummary: DataBreakdown[]
-  speciesSummary: DataBreakdown[]
-  speciesGenomeSummary: DataBreakdown[]
+  dataSummary: DataBreakdown[];
+  speciesSummary: DataBreakdown[];
+  speciesGenomeSummary: DataBreakdown[];
   summary: {
-    children: number,
-    childrenData: number,
-    childrenGenomes: number
-    species: number,
-    speciesData: number,
-    speciesGenomes: number,
-  }
+    children: number;
+    childrenData: number;
+    childrenGenomes: number;
+    species: number;
+    speciesData: number;
+    speciesGenomes: number;
+  };
 };
 
 type TaxonResults = {
-  taxon: Taxonomy,
-}
-
+  taxon: Taxonomy;
+};
 
 export default function ShowStats() {
   const taxonResults = useQuery<TaxonResults>(GET_TAXON);
-  const taxon = taxonResults.data?.taxon
+  const taxon = taxonResults.data?.taxon;
 
   const thresholds = [
     { name: "low", color: "#f47625", start: 0, end: 50 },
     { name: "decent", color: "#febb1e", start: 50, end: 75 },
     { name: "great", color: "#97bc5d", start: 75, end: 100 },
-  ]
+  ];
 
-  const speciesGenomes = taxon?.speciesGenomeSummary.filter(i => i.genomes > 0).map(summary => {
-    const linkName = encodeURIComponent(summary.name.replaceAll(' ', '_'));
-    return {
-      name: summary.name || '',
-      value: summary.genomes,
-      href: `/species/${linkName}`,
-    }
-  }).sort((a, b) => b.value - a.value)
+  const speciesGenomes = taxon?.speciesGenomeSummary
+    .filter((i) => i.genomes > 0)
+    .map((summary) => {
+      const linkName = encodeURIComponent(summary.name.replaceAll(" ", "_"));
+      return {
+        name: summary.name || "",
+        value: summary.genomes,
+        href: `/species/${linkName}`,
+      };
+    })
+    .sort((a, b) => b.value - a.value);
 
-  const genomePercentile = taxon && (taxon.summary.speciesGenomes / taxon.summary.species) * 100;
+  const genomePercentile =
+    taxon && (taxon.summary.speciesGenomes / taxon.summary.species) * 100;
 
   return (
-    <Paper radius="20px" mr={50} style={{ position: "absolute", top: 200, right: 0, width: "650px" }}>
+    <Paper radius="lg" style={{ top: 200, right: 0, width: "650px" }}>
       <LoadOverlay visible={taxonResults.loading} />
       <Grid p={20}>
         <Grid.Col span={6} mb={10}>
           <Title order={5}>Data summary</Title>
           <Stack>
-            <Text fz="sm" fw={300}>Percentage of species with genomes</Text>
-            {taxon && <TachoChart h={130} thresholds={thresholds} value={Math.round(genomePercentile || 0)} />}
+            <Text fz="sm" fw={300}>
+              Percentage of species with genomes
+            </Text>
+            {taxon && (
+              <TachoChart
+                h={130}
+                thresholds={thresholds}
+                value={Math.round(genomePercentile || 0)}
+              />
+            )}
           </Stack>
         </Grid.Col>
 
@@ -94,17 +106,24 @@ export default function ShowStats() {
           <Title order={5}>Taxonomic breakdown</Title>
 
           <DataTable my={2}>
-
             <DataTableRow label="Number of species/OTUs">
-              <DataField value={Humanize.formatNumber(taxon?.summary.species || 0)} />
+              <DataField
+                value={Humanize.formatNumber(taxon?.summary.species || 0)}
+              />
             </DataTableRow>
 
             <DataTableRow label="Species with genomes">
-              <DataField value={Humanize.formatNumber(taxon?.summary.speciesGenomes || 0)} />
+              <DataField
+                value={Humanize.formatNumber(
+                  taxon?.summary.speciesGenomes || 0
+                )}
+              />
             </DataTableRow>
 
             <DataTableRow label="Species with data">
-              <DataField value={Humanize.formatNumber(taxon?.summary.speciesData || 0)} />
+              <DataField
+                value={Humanize.formatNumber(taxon?.summary.speciesData || 0)}
+              />
             </DataTableRow>
           </DataTable>
 
@@ -112,21 +131,87 @@ export default function ShowStats() {
             <Attribute
               label="Species with most genomes"
               value={speciesGenomes && speciesGenomes[0]?.name}
-              href={`/species/${speciesGenomes && speciesGenomes[0]?.name?.replaceAll(' ', '_')}/taxonomy`}
+              href={`/species/${
+                speciesGenomes && speciesGenomes[0]?.name?.replaceAll(" ", "_")
+              }/taxonomy`}
             />
           </Stack>
         </Grid.Col>
 
         <Grid.Col span={12}>
           <Stack>
-            <Text fz="sm" fw={300}>Species with genomes</Text>
-            {speciesGenomes && <BarChart h={250} data={speciesGenomes.slice(0, 10)} spacing={0.1} labelWidth={200} />}
+            <Text fz="sm" fw={300}>
+              Species with genomes
+            </Text>
+            {speciesGenomes && (
+              <BarChart
+                h={250}
+                data={speciesGenomes.slice(0, 10)}
+                spacing={0.1}
+                labelWidth={200}
+              />
+            )}
           </Stack>
         </Grid.Col>
         <Grid.Col span={12}>
-          <Text fz="xs" fw={300}>Note: these statistics summarise the content indexed within ARGA.  The values relate to the species deemed relevant to Australia (either by endemicity or economic and social value), and for repositories that are indexed by ARGA.  The values may not be indicative of global values for all research.</Text>
+          <Text fz="xs" fw={300}>
+            Note: these statistics summarise the content indexed within ARGA.
+            The values relate to the species deemed relevant to Australia
+            (either by endemicity or economic and social value), and for
+            repositories that are indexed by ARGA. The values may not be
+            indicative of global values for all research.
+          </Text>
         </Grid.Col>
       </Grid>
     </Paper>
-  )
+    // <Paper radius="20px" mr={50} style={{ position: "absolute", top: 200, right: 0, width: "650px" }}>
+    //   <LoadOverlay visible={taxonResults.loading} />
+    //   <Grid p={20}>
+    //     <Grid.Col span={6} mb={10}>
+    //       <Title order={5}>Data summary</Title>
+    //       <Stack>
+    //         <Text fz="sm" fw={300}>Percentage of species with genomes</Text>
+    //         {taxon && <TachoChart h={130} thresholds={thresholds} value={Math.round(genomePercentile || 0)} />}
+    //       </Stack>
+    //     </Grid.Col>
+
+    //     <Grid.Col span={6} mb={10}>
+    //       <Title order={5}>Taxonomic breakdown</Title>
+
+    //       <DataTable my={2}>
+
+    //         <DataTableRow label="Number of species/OTUs">
+    //           <DataField value={Humanize.formatNumber(taxon?.summary.species || 0)} />
+    //         </DataTableRow>
+
+    //         <DataTableRow label="Species with genomes">
+    //           <DataField value={Humanize.formatNumber(taxon?.summary.speciesGenomes || 0)} />
+    //         </DataTableRow>
+
+    //         <DataTableRow label="Species with data">
+    //           <DataField value={Humanize.formatNumber(taxon?.summary.speciesData || 0)} />
+    //         </DataTableRow>
+    //       </DataTable>
+
+    //       <Stack mx={10} mt={5}>
+    //         <Attribute
+    //           label="Species with most genomes"
+    //           value={speciesGenomes && speciesGenomes[0]?.name}
+    //           href={`/species/${speciesGenomes && speciesGenomes[0]?.name?.replaceAll(' ', '_')}/taxonomy`}
+    //         />
+    //       </Stack>
+    //     </Grid.Col>
+
+    //     <Grid.Col span={12}>
+    //       <Stack>
+    //         <Text fz="sm" fw={300}>Species with genomes</Text>
+    //         {speciesGenomes && <BarChart h={250} data={speciesGenomes.slice(0, 10)} spacing={0.1} labelWidth={200} />}
+    //       </Stack>
+    //     </Grid.Col>
+    //     <Grid.Col span={12}>
+    //       <Text fz="xs" fw={300}>Note: these statistics summarise the content indexed within ARGA.  The values relate to the species deemed relevant to Australia (either by endemicity or economic and social value), and for repositories that are indexed by ARGA.  The values may not be indicative of global values for all research.</Text>
+    //     </Grid.Col>
+    //   </Grid>
+    // </Paper>
+  );
 }
