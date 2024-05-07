21c89e361eab16e534e4d46b5aa8458af8aee26b
Caitlin <crams27@gmail.com>
2024-05-07 17:45:12 +1000

    added new UI update messages to changelog

--- src/components/recent-updates-container.tsx
+++ src/components/recent-updates-container.tsx
@@ -1,19 +1,19 @@
 import { Grid, Text, ScrollArea, Paper } from "@mantine/core";
-import updatesData from "../../public/recent-updates.json";
+import updatesData from "../../public/public-changelog.json";
 
 export default function RecentUpdatesContainer() {
   const changelog = updatesData.changelog;
   return (
     <Paper w={400} h={425} bg="midnight.6" radius="lg" shadow="sm">
       <ScrollArea h="100%" offsetScrollbars scrollbars="y">
-        <Grid p={30}>
+        <Grid p={30} justify="center" columns={3}>
           {changelog.map((update) => {
             return (
               <>
-                <Grid.Col key={update.date} span={6}>
+                <Grid.Col key={update.date} span={1}>
                   <Text c="shellfish.6">{update.date}</Text>
                 </Grid.Col>
-                <Grid.Col span={6}>
+                <Grid.Col span={2}>
                   {update.updates.map((updateMsg) => {
                     return (
                       <Text key={update.date} c="midnight.2">
