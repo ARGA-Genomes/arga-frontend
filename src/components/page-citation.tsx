9469f95b3d99b2852d169fd5b9f706870dde44b0
Jack Brinkman <jack.brinkman@csiro.au>
2024-10-25 11:48:58 +1000

    First-pass page citation implementation

---
+++ src/components/page-citation.tsx
@@ -0,0 +1,87 @@
+import { Container, Paper, Stack, Text } from "@mantine/core";
+import { DataTable, DataTableRow } from "./data-table";
+import { useParams, usePathname } from "next/navigation";
+import { gql, useQuery } from "@apollo/client";
+import { LoadOverlay } from "./load-overlay";
+import { Taxonomy } from "@/app/type";
+import { MAX_WIDTH } from "@/app/constants";
+
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+type QueryResults = {
+  species: {
+    taxonomy: Taxonomy[];
+  };
+};
+
+const GET_SPECIES = gql`
+  query Species($canonicalName: String) {
+    species(canonicalName: $canonicalName) {
+      taxonomy {
+        canonicalName
+        vernacularGroup
+        authorship
+      }
+    }
+  }
+`;
+
+export function PageCitation() {
+  const params = useParams();
+  const location = usePathname();
+
+  const { loading, error, data } = useQuery<QueryResults>(GET_SPECIES, {
+    variables: {
+      canonicalName: (params.name as string).replaceAll("_", " "),
+    },
+  });
+
+  const taxonomy = data?.species.taxonomy[0];
+  const date = new Date();
+
+  if (error) {
+    return <Text>Error : {error.message}</Text>;
+  }
+
+  return (
+    <Paper pb="lg">
+      <LoadOverlay visible={loading} />
+      <Container maw={MAX_WIDTH}>
+        <Paper p="md" radius="lg" withBorder>
+          <Stack>
+            <Text size="lg" fw="bold">
+              Page information
+            </Text>
+            <DataTable>
+              <DataTableRow label="Citation">
+                <Text fw={600} fz="sm" c="midnight.7">
+                  Australian Reference Genome Atlas {date.getFullYear()},{" "}
+                  {`<i>${taxonomy?.canonicalName} ${taxonomy?.authorship}, Taxonomy</i>`}
+                  , The Australian Reference Genome Atlas, accessed{" "}
+                  {date.getDate()} {months[date.getMonth()]}{" "}
+                  {date.getFullYear()},
+                  <br />
+                  {"<"}https://app.arga.org.au{location}
+                  {">"}
+                </Text>
+              </DataTableRow>
+            </DataTable>
+          </Stack>
+        </Paper>
+      </Container>
+    </Paper>
+  );
+}
