d5624e9c8779bfc8ca72008fb7f2214d55ddc509
Jack Brinkman <jack.brinkman@csiro.au>
2024-10-25 15:19:28 +1000

    Fixed citation format and page name, added copy button

--- src/components/page-citation.tsx
+++ src/components/page-citation.tsx
@@ -1,10 +1,22 @@
-import { Container, Paper, Stack, Text } from "@mantine/core";
+import {
+  ActionIcon,
+  Container,
+  Flex,
+  Paper,
+  Stack,
+  Text,
+  Tooltip,
+} from "@mantine/core";
 import { DataTable, DataTableRow } from "./data-table";
 import { useParams, usePathname } from "next/navigation";
 import { gql, useQuery } from "@apollo/client";
 import { LoadOverlay } from "./load-overlay";
 import { Taxonomy } from "@/app/type";
 import { MAX_WIDTH } from "@/app/constants";
+import * as Humanize from "humanize-plus";
+import { useClipboard } from "@mantine/hooks";
+import { useRef } from "react";
+import { IconCopy } from "@tabler/icons-react";
 
 const months = [
   "Jan",
@@ -42,6 +54,9 @@ const GET_SPECIES = gql`
 export function PageCitation() {
   const params = useParams();
   const location = usePathname();
+  const page = Humanize.capitalize(location.split("/").pop() || "");
+  const clipboard = useClipboard({ timeout: 500 });
+  const citation = useRef<HTMLParagraphElement>(null);
 
   const { loading, error, data } = useQuery<QueryResults>(GET_SPECIES, {
     variables: {
@@ -67,21 +82,34 @@ export function PageCitation() {
             </Text>
             <DataTable>
               <DataTableRow label="Citation">
-                <Text fw={600} fz="sm" c="midnight.7">
-                  Australian Reference Genome Atlas {date.getFullYear()},{" "}
-                  <i>
-                    {taxonomy?.canonicalName} {taxonomy?.authorship}, Taxonomy
-                  </i>
-                  , The Australian Reference Genome Atlas, accessed{" "}
-                  {date.getDate()} {months[date.getMonth()]}{" "}
-                  {date.getFullYear()},{" "}
-                  <a
-                    aria-label="Citation Link"
-                    href={`https://app.arga.org.au${location}`}
-                  >
-                    https://app.arga.org.au{location}
-                  </a>
-                </Text>
+                <Flex align="center" gap="sm">
+                  <Tooltip label={clipboard.copied ? "Copied" : "Copy"}>
+                    <ActionIcon
+                      color="midnight.8"
+                      size="sm"
+                      onClick={() =>
+                        clipboard.copy(citation.current?.innerText)
+                      }
+                    >
+                      <IconCopy size={14} />
+                    </ActionIcon>
+                  </Tooltip>
+                  <Text fw={600} fz="sm" c="midnight.7" ref={citation}>
+                    Australian Reference Genome Atlas. {date.getFullYear()}.{" "}
+                    <i>
+                      {taxonomy?.canonicalName} {taxonomy?.authorship}, {page}
+                    </i>
+                    , The Australian Reference Genome Atlas. Accessed at:{" "}
+                    <a
+                      aria-label="Citation Link"
+                      href={`https://app.arga.org.au${location}`}
+                    >
+                      https://app.arga.org.au{location}
+                    </a>{" "}
+                    on {date.getDate()}-{date.getMonth() + 1}-
+                    {date.getFullYear()}
+                  </Text>
+                </Flex>
               </DataTableRow>
             </DataTable>
           </Stack>
