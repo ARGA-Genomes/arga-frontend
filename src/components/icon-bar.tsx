fd124c782fe52cbeffdd65f15a84ad97054587a6
Jack Brinkman <jack.brinkman@csiro.au>
2024-10-25 15:23:50 +1000

    Fixed build error with icon bar

--- src/components/icon-bar.tsx
+++ src/components/icon-bar.tsx
@@ -487,8 +487,6 @@ export default function IconBar({
   traits,
   attributes,
 }: IconBarProps) {
-  if (!taxonomy) return null;
-
   const taxonomyHeaderIcons = [taxonomy.vernacularGroup];
   isLichen(taxonomy.source) ? taxonomyHeaderIcons.push("LICHENS") : undefined;
 
