e9e66eebce4bb27734dab3abf5ad37dce68baf90
Jack Brinkman <jack.brinkman@csiro.au>
2024-11-26 12:58:06 +1000

    Migrated specimens and taxonomy pages

--- src/app/species/[name]/resources/page.tsx
+++ src/app/species/[name]/resources/page.tsx
@@ -1,40 +1,39 @@
-'use client';
+"use client";
 
 import { gql, useQuery } from "@apollo/client";
-import { Box, Paper, Text } from '@mantine/core';
+import { Box, Paper, Text } from "@mantine/core";
 import { CommonGenome } from "@/app/type";
-import GenomeTable from "../commonGenomeRecordTable";
+import GenomeTable from "./commonGenomeRecordTable";
 import { LoadOverlay } from "@/components/load-overlay";
 
-
 const GET_RESOURCES = gql`
-query SpeciesResources($canonicalName: String) {
-  species(canonicalName: $canonicalName) {
-    data {
-      canonicalName
-      type
-      dataResource
-      recordedBy
-      license
-      provenance
-      eventDate
-      accession
-      accessionUri
-      refseqCategory
-      coordinates {
-        latitude
-        longitude
+  query SpeciesResources($canonicalName: String) {
+    species(canonicalName: $canonicalName) {
+      data {
+        canonicalName
+        type
+        dataResource
+        recordedBy
+        license
+        provenance
+        eventDate
+        accession
+        accessionUri
+        refseqCategory
+        coordinates {
+          latitude
+          longitude
+        }
       }
     }
   }
-}`;
+`;
 
 type QueryResults = {
   species: {
-    data: CommonGenome[]
-  },
+    data: CommonGenome[];
+  };
 };
-
 
 export default function Resources({ params }: { params: { name: string } }) {
   const canonicalName = params.name.replaceAll("_", " ");
@@ -46,16 +45,18 @@ export default function Resources({ params }: { params: { name: string } }) {
   });
 
   if (error) {
-    return (<Text>Error : {error.message}</Text>);
+    return <Text>Error : {error.message}</Text>;
   }
 
   return (
     <Box pos="relative">
       <LoadOverlay visible={loading} />
 
-      <Text style={{padding: 25}} c="white">All Genome Sequence Records</Text>
+      <Text style={{ padding: 25 }} c="white">
+        All Genome Sequence Records
+      </Text>
       <Paper radius="lg" py={25}>
-        { data ? <GenomeTable records={data.species.data} /> : null }
+        {data ? <GenomeTable records={data.species.data} /> : null}
       </Paper>
     </Box>
   );
