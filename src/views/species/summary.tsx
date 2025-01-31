a6f7d96fe3c7f0cd25be46542dc6edfa3ede1177
Caitlin <crams27@gmail.com>
2025-01-31 16:38:37 +1000

    redirected higher taxonomy button URLs to correct taxonomic system from ALA one if they are not animals ie. plantae, fungi

--- src/views/species/summary.tsx
+++ src/views/species/summary.tsx
@@ -304,36 +304,20 @@ function Classification({ taxonomy }: { taxonomy: Taxonomy }) {
     (a, b) => b.depth - a.depth
   );
 
-  const hierarchy = classification;
+  const taxonRankMappings: Record<string, string> = {
+    KINGDOM: "REGNUM",
+    PHYLUM: "DIVISION",
+    SUBPHYLUM: "SUBDIVISION",
+    CLASS: "CLASSIS",
+    SUBCLASS: "SUBCLASSIS",
+    ORDER: "ORDO",
+    SUPERORDER: "SUPERORDO",
+    FAMILY: "FAMILIA",
+    GENUS: "GENUS",
+    SPECIES: "SPECIES",
+  };
 
-  // quick fix of plant, fungi and chromist taxon ranks
-  // TO-DO: needs to be fixed properly in the backend
-  // const taxonRankMappings: Record<string, string> = {
-  //   KINGDOM: "REGNUM",
-  //   PHYLUM: "DIVISION",
-  //   SUBPHYLUM: "SUBDIVISION",
-  //   CLASS: "CLASSIS",
-  //   SUBCLASS: "SUBCLASSIS",
-  //   ORDER: "ORDO",
-  //   SUPERORDER: "SUPERORDO",
-  //   FAMILY: "FAMILIA",
-  //   GENUS: "GENUS",
-  //   SPECIES: "SPECIES",
-  // };
-
-  // let hierarchy;
-
-  // if (
-  //   classification?.[0].canonicalName !== "Animalia" &&
-  //   classification?.[0].canonicalName !== "Protista"
-  // ) {
-  //   hierarchy = classification?.map((item) => ({
-  //     ...item,
-  //     rank: taxonRankMappings[item.rank.toUpperCase()] || item.rank,
-  //   }));
-  // } else {
-  //   hierarchy = classification;
-  // }
+  const isAnimalia = classification?.[1]?.canonicalName === "Animalia";
 
   return (
     <Paper radius={16} p="md" withBorder>
@@ -358,20 +342,38 @@ function Classification({ taxonomy }: { taxonomy: Taxonomy }) {
 
       <Group>
         {error && <Text>{error.message}</Text>}
-        {hierarchy?.map((node, idx) => (
-          <AttributePill
-            key={idx}
-            labelColor="midnight.8"
-            popoverDisabled
-            hoverColor="midnight.0"
-            label={Humanize.capitalize(node.rank.toLowerCase())}
-            value={node.canonicalName}
-            href={`/${node.rank.toLowerCase()}/${node.canonicalName}`}
-            icon={IconArrowUpRight}
-            showIconOnHover
-            miw={100}
-          />
-        ))}
+        {classification?.map((node, idx) =>
+          isAnimalia ? (
+            <AttributePill
+              key={idx}
+              labelColor="midnight.8"
+              popoverDisabled
+              hoverColor="midnight.0"
+              label={Humanize.capitalize(node.rank.toLowerCase())}
+              value={node.canonicalName}
+              href={`/${node.rank.toLowerCase()}/${node.canonicalName}`}
+              icon={IconArrowUpRight}
+              showIconOnHover
+              miw={100}
+            />
+          ) : (
+            <AttributePill
+              key={idx}
+              labelColor="midnight.8"
+              popoverDisabled
+              hoverColor="midnight.0"
+              label={Humanize.capitalize(node.rank.toLowerCase())}
+              value={node.canonicalName}
+              href={`/${
+                taxonRankMappings[node.rank.toUpperCase()]?.toLowerCase() ||
+                node.rank.toLowerCase()
+              }/${node.canonicalName}`}
+              icon={IconArrowUpRight}
+              showIconOnHover
+              miw={100}
+            />
+          )
+        )}
       </Group>
     </Paper>
   );
