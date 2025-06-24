f29cbaf84d86df9cf4ef8619d946d65d28653d53
Goran Sterjov <secure@goran.email>
2025-06-24 16:28:05 +1000

    use new specimen endpoint for taxonomy page. remove subspecies pages

--- src/app/species/[name]/specimens/[accession]/page.tsx
+++ src/app/species/[name]/specimens/[accession]/page.tsx
@@ -1,6 +1,3 @@
-import SpecimenAccession from "@/views/species/specimens/accession";
-
-export default async function Page(props: { params: Promise<{ accession: string }> }) {
-  const params = await props.params;
-  return <SpecimenAccession params={params} />;
+export default async function Page() {
+  return <></>;
 }
--- src/app/subspecies/[name]/whole_genomes/[accession]/page.tsx
+++
@@ -1,5 +0,0 @@
-import AccessionPage, { AccessionPageProps } from "@/views/species/whole_genomes/accession";
-
-export default function Page(props: AccessionPageProps) {
-  return <AccessionPage {...props} />;
-}
